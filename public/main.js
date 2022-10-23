6eca71c17c8e672773cfa75eee6a3997a89aafa7
NevenGerencir <gerencirn@gmail.com>
2022-10-23 20:36:44 +0200

    finished delete,put

---
+++ public/main.js
@@ -0,0 +1,54 @@
+const itemsUnCom = [...document.querySelectorAll('.uncompleted')]
+const itemsCom = [...document.querySelectorAll('.completed')]
+const trash = [...document.getElementsByClassName('fa fa-trash')]
+
+
+trash.forEach(element => element.addEventListener('click',deleteItem))
+itemsUnCom.forEach(element=>element.addEventListener('click',complete))
+itemsCom.forEach(element=>element.addEventListener('click',undo))
+
+
+async function deleteItem(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    console.log(itemText)
+    response = await fetch('deleteItem',{
+        method: 'delete',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          'itemFromJS': itemText
+        })
+      })
+      data = await response.json()
+      console.log(data)
+        location.reload()
+}
+
+
+async function complete(){
+    const itemText = this.innerText
+    response = await fetch('completeItem',{
+        method: 'put',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          'itemFromJS': itemText
+        })
+      })
+      data = await response.json()
+      console.log(data)
+        location.reload()
+}
+
+async function undo(){
+    const itemText = this.innerText
+    response = await fetch('undoItem',{
+        method: 'put',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          'itemFromJS': itemText
+        })
+      })
+      data = await response.json()
+      console.log(data)
+        location.reload()
+}
+
--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const { response } = require('express');
 const express=require('express');
 const app=express();
 const PORT = 8000;
+
 MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
+
 app.use(express.static('./public'))
 app.set('view engine','ejs')
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -31,6 +34,7 @@ app.get('/',(request,res) => {
     })
     .catch(error=>console.log(error))
 })
+
 app.post('/send',(req,res)=>{
     collection.insertOne({
         todo: req.body.thingToDo,
@@ -40,7 +44,52 @@ app.post('/send',(req,res)=>{
     .then(result =>{
 res.redirect('/')
     })
+
 })
+app.put('/completeItem',(req,res)=>{
+    console.log(req.body)
+    collection.updateOne({todo: req.body.itemFromJS},{
+        $set: {
+            completed: true
+          }
+    },{
+        sort: {_id: -1},
+        upsert: false
+    })
+    .then(result => {
+        console.log('Marked completed : true')
+        res.json('Marked completed : true')
+    })
+    .catch(error => console.error(error))
+})
+
+
+app.put('/undoItem',(req,res)=>{
+    console.log(req.body)
+    collection.updateOne({todo: req.body.itemFromJS},{
+        $set: {
+            completed: false
+          }
+    },{
+        sort: {_id: -1},
+        upsert: false
+    })
+    .then(result => {
+        console.log('Marked completed : false')
+        res.json('Marked completed : false')
+    })
+    .catch(error => console.error(error))
+})
+
+app.delete('/deleteItem',(req,res)=>{
+    console.log(req.body)
+    collection.deleteOne({todo:req.body.itemFromJS})
+    .then(result => {
+        res.json('Deleted')
+    })
+    .catch(error => console.error(error))
+})
+
 app.listen(PORT,()=>{
     console.log(`Listening on post ${PORT}`)
 })
